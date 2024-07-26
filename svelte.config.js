885d9e2655a40e652355fbbb070e60e71f3805eb
TamLeal <tamerleal@hotmail.com>
2024-07-26 14:28:23 -0400

    ddss

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,16 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		prerender: {
-			handleMissingId: 'ignore'
-		}
-	},
-	preprocess: vitePreprocess()
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    })
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
